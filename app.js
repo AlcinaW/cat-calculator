cf41cb879ba323df8fe0f9c9e62fd42aec5023af
AlcinaW <white_kaitou@hotmail.com>
2016-02-06 00:56:00 -0500

    JS logic

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var meow = document.getElementById('screen');
 // Puts text to screen, if "C", clears screen and values
 function addToScreen(x) {
 	meow.value += x;
-	if (x === "C") {
+	if (x === "ac") {
 		meow.value = "";
 	}
 }
 
+// Backspace one number from the end using length
+function backspace() {
+	var number = meow.value;
+	var len = number.length-1;
+	var newNumber = number.substring(0, len);
+	meow.value=newNumber;
+}
+
+// Does the math 
 function answer() {
 	x = meow.value;
 	x = eval(x);
